46bbf6915e18c74a514cd44c2bb69c5d6334337a
Bintang Ramadhan <bintang4974.bintangramadhan4974@gmail.com>
2022-09-02 17:35:37 +0700

    login & create data

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'UNAUTHORIZED!'
+            ], 401);
+        }
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'message' => 'success!',
+            'token' => $token,
+            'data' => $user
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Success Logout!'
+        ], 200);
+    }
+}
---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function get()
+    {
+        $product = Product::get();
+
+        return response()->json([
+            'message' => 'success!',
+            'data' => $product
+        ], 200);
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'nama_product' => 'required',
+            'harga_product' => 'required',
+            'stok_product' => 'required',
+            'deskripsi_product' => 'required',
+        ]);
+
+        $product = new Product();
+        $product->nama_product = $request->nama_product;
+        $product->harga_product = $request->harga_product;
+        $product->stok_product = $request->stok_product;
+        $product->deskripsi_product = $request->deskripsi_product;
+        $product->save();
+
+        return response()->json([
+            'message' => 'success!',
+            'data' => $product
+        ], 200);
+    }
+}
